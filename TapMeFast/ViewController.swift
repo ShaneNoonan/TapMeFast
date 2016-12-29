1d03221c8e87515222b045c6b88c91adaefc28ae
ShaneNoonan <ShaneNoonan@MacBook-Pro.local>
2016-12-29 19:32:41 +0000

    TapMeFast is a fun easy game to see how many taps you can get in 10 seconds. Created in Swift 3 using nothing but a NSTimer, Buttons and Labels

--- TapMeFast/ViewController.swift
+++ TapMeFast/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var timerLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var tapMe: UIButton!
+    @IBOutlet var startGame: UIButton!
+    
+    var timer = Timer()
+    
+    var tapInt = 0
+    var timerInt = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        timerInt = 10
+        tapInt = 0
+        
+        scoreLabel.text = String(tapInt)
+        timerLabel.text = String(timerInt)
+        
+        tapMe.isEnabled = false
+        tapMe.alpha = 0.5
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +39,53 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tapMeButton(_ sender: Any) {
+        
+        tapInt += 1
+        scoreLabel.text = String(tapInt)
+    }
 
+    @IBAction func startGameButton(_ sender: Any) {
+        if timerInt == 10 {
+            tapInt = 0
+            scoreLabel.text = String(tapInt)
+            
+            tapMe.isEnabled = true
+            tapMe.alpha = 1
+            
+            startGame.isEnabled = false
+            startGame.alpha = 0.5
+            
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.startCounter), userInfo: nil, repeats: true)
+        }
+        
+        if timerInt == 0 {
+            
+            timerInt = 10
+            tapInt = 0
+            
+            scoreLabel.text = String(tapInt)
+            timerLabel.text = String(timerInt)
+            
+            startGame.setTitle("START", for: [])
+        }
+    }
+    
+    func startCounter() {
+        
+        timerInt -= 1
+        timerLabel.text = String(timerInt)
+        
+        if timerInt == 0 {
+            timer.invalidate()
+            
+            tapMe.isEnabled = false
+            tapMe.alpha = 0.5
+            
+            startGame.isEnabled = true
+            startGame.alpha = 1
+            startGame.setTitle("RESTART", for: [])
+        }
+    }
 }
 
